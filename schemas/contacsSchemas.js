898d7863030576f4dd71a7d655e78af547790946
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2024-05-20 00:28:51 +0300

    add code for mongo

--- schemas/contacsSchemas.js
+++ schemas/contacsSchemas.js
@@ -1,13 +1,23 @@
-import Joi from "joi";
-
-export const createContactSchema = Joi.object({
-  name: Joi.string().required().min(5),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
-
-export const updateContactSchema = Joi.object({
-  name: Joi.string().required().min(5),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+import mongoose from "mongoose";
+const contactsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+export default mongoose.model("contact", contactsSchema);
