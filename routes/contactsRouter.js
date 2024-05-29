bd6246fe201632b68ce35b6aa3eb690af6d94128
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2024-05-29 18:06:58 +0300

    solve problems01

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -7,19 +7,19 @@ import {
   updateContact,
   updateContactFavorite,
 } from "../controllers/contactsControllers.js";
-//import auth from "../middleware/auth.js";
+import auth from "../middleware/auth.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", getAllContacts);
+contactsRouter.get("/", auth, getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", auth, getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", auth, deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", auth, createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", auth, updateContact);
 
-contactsRouter.patch("/:id/favorite", updateContactFavorite);
+contactsRouter.patch("/:id/favorite", auth, updateContactFavorite);
 export default contactsRouter;
